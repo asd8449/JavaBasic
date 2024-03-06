7ad401a51d380691d17e1cce746ac316c4870002
asd8449 <162385999+asd8449@users.noreply.github.com>
2024-03-07 08:35:37 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,18 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "박병준";
+		int myAge = 25;
+		
+		System.out.println("이름 : " + myName);
+		System.out.println("나이 : " + myAge);
+		
+		System.out.print("\n");
+		System.out.print("이름 : " + myName);
+		System.out.print("  나이 : " + myAge);
+	}
+
+}
