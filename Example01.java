244bd077db9d828cf0da101ad573410ebc5dd1f6
asd8449 <162385999+asd8449@users.noreply.github.com>
2024-03-11 13:36:41 +0900

    Add files via upload

---
+++ Example01.java
@@ -0,0 +1,27 @@
+package print;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String compileLang = "Java";
+		String scriptLang = "JavaScript";
+
+		System.out.println("나는 박병준입니다.");
+		System.out.println(compileLang + " Program");
+		System.out.println(scriptLang + " Program");
+		System.out.println("하루는 " + 24 * 60 + "분 입니다.");
+		System.out.println();
+
+		String program = "Program";
+		System.out.println(compileLang + " " + program);
+		System.out.println(scriptLang + " " + program);
+		System.out.println();
+
+		int a = 10;
+		int b = 15;
+		System.out.println(a + " + " + b + " = " + a + b);
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+
+}
